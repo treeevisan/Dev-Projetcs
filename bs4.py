c528aee56bcfd059ecb9ba06337ce0d990d6bc3c
treeevisan <renantreviz09@gmail.com>
2024-01-23 22:26:44 -0300

    commit

---
+++ bs4.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.google.com/search?q=ceo+google'
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+    
+
+requisicao = requests.get(url, headers=headers)
+
+print(requisicao)
+#print(requisicao.text)
+
+site = BeautifulSoup(requisicao.text, 'html.parser')
+#print(site.prettify())
+
+h3 = site.find_all('h3', limit = 5)
+
+print(h3)
+
+
+pesquisa2 = site.find_all(class_='qLRx3b tjvcx GvPZzd cHaqb', role='text')
+print(pesquisa2)
+
+
+h3 = site.find('h3', limit = 2)
+print(h3)
+
+
+import webbrowser
+
+webbrowser.open_new('https://www.google.com/search?q=ceo+google')
+webbrowser.close
---
+++ project.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import webbrowser as wb
+import pyautogui as pyg
+import time
+
+
+
+def concat():
+    
+
+def pesquisar():
+    pass
+    time.sleep(3)
+
+def abrir_nav(url):
+    wb.open_new(url)
+    time.sleep(2)
+
+
+def abrir_aba(url):
+    wb.open_new(url)
+    time.sleep(2)
+
+def fechar_nav():
+    pyg.hotkey('ctrl', 'w')
